6d5152aacf1a43c6fdca5546f45cc1060b4a44d9
oeshiro <welliw777@gmail.com>
2015-12-01 04:58:52 +0300

    Initial commit.

---
+++ Creature.py
@@ -0,0 +1,7 @@
+class Creature:
+    DEFAULT_HP = 100
+    DEFAULT_LOSS_PER_TURN = 1
+
+    def __init__(self, hp=DEFAULT_HP,
+                 loss_per_turn=DEFAULT_LOSS_PER_TURN):
+        self.hitpoints = hp
---
+++ Prey.py
@@ -0,0 +1,7 @@
+import Creature
+
+
+class Prey(Creature):
+
+    def __str__(self):
+        return "x"
