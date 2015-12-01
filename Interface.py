984288703045e91d12bef41ab6ac331a1d513f8f
Ziro-Sei <FJMK2011@gmail.com>
2015-12-01 07:13:35 +0400

    Added generation + reworked input again + comments

--- Interface.py
+++ Interface.py
@@ -1,8 +1,10 @@
 import os
 import Field
-
+from math import floor
+from random import randint
 
 class Interface:
+    #constructor
     def __init__(self):
         self.width = 0
         self.height = 0
@@ -11,78 +13,115 @@ class Interface:
         self.prey_number = 0
         self.generate()
 
+    #draws map in console
     def draw(self, field):
         os.system('cls')
-        title = "The Fabulous Ocean Simulation Project"
-        print("=" * ((field.width - len(title)) // 2) + title + "=" * (
-            (field.width - len(title)) // 2 + (field.width - len(title)) % 2))
-        subtitle = "2015"
-        print("=" * ((field.width - len(subtitle)) // 2) + subtitle + "=" * (
-            (field.width - len(subtitle)) // 2 + (field.width - len(subtitle)) % 2))
+        self.draw_top_bar(field)
         for i in range(len(field.objects)):
             print(''.join([str(obj) for obj in field.objects[i]]))
         print("Creatures: " + str(field.creatures_count()) + ". Predators: " + str(
             field.predators_count()) + ". Preys: " + str(field.preys_count())
 + ". ")
 
+    #draws top bar
+    def draw_top_bar(self, field):
+        title = "The Fabulous Ocean Simulation Project"
+        print("=" * ((field.width - len(title)) // 2) + title + "=" * (
+            (field.width - len(title)) // 2 + (field.width - len(title)) % 2))
+        subtitle = "2015"
+        print("=" * ((field.width - len(subtitle)) // 2) + subtitle + "=" * (
+            (field.width - len(subtitle)) // 2 + (field.width - len(subtitle)) % 2))
+
+    #generates new field
     def generate(self):
         os.system('cls')
         print("GENERATING NEW MAP")
         self.enter_params()
+        field = Field(self.width, self.height)
+        obstacles = floor(self.width*self.height*self.obstacle_density/100)
+        predators = self.predator_number
+        preys = self.prey_number
+        empty = self.width*self.height - obstacles - predators - preys
+        for i in range(self.height):
+            for j in range(self.height):
+                rand = randint(0, obstacles + predators + preys + empty - 1)
+                if rand < obstacles:
+                    obstacles -= 1
+                    field.add_obstacle(j, i)
+                elif rand < predators + obstacles:
+                    predators -= 1
+                    field.add_predator(j, i)
+                elif rand < preys + predators + obstacles:
+                    preys -= 1
+                    field.add_prey(j, i)
 
+    #asks for params
     def enter_params(self):
         self.enter_width()
         self.enter_height()
         self.enter_obstacle_density()
         self.enter_predator_number()
         self.enter_prey_number()
+        if self.width*self.height*(1-self.obstacle_density/100)<self.predator_number+self.prey_number:
+            print("   Your whole input is wrong!!")
+            self.enter_params()
 
+    #asks for width
     def enter_width(self):
-        while True:
-            print("   Enter width in pixels: ", end='')
-            try:
-                self.width = int(input())
-            except:
-                self.width = 0
-            if self.width > 0:
-                break
+        print("   Enter width in pixels: ", end='')
+        try:
+            self.width = int(input())
+        except:
+            print("   Your input is wrong!!")
+            self.enter_width()
+        if self.width <=0:
+            print("   Your input is wrong!!")
+            self.enter_width()
 
+    #asks for height
     def enter_height(self):
-        while True:
-            print("   Enter height in pixels: ", end='')
-            try:
-                self.height = int(input())
-            except:
-                self.height = 0
-            if self.height > 0:
-                break
+        print("   Enter height in pixels: ", end='')
+        try:
+            self.height = int(input())
+        except:
+            print("   Your input is wrong!!")
+            self.enter_height()
+        if self.height <=0:
+            print("   Your input is wrong!!")
+            self.enter_height()
 
+    #asks for obstacle density
     def enter_obstacle_density(self):
-        while True:
-            print("   Enter obstacle density in percentage: ", end='')
-            try:
-                self.obstacle_density = float(input())
-            except:
-                self.obstacle_density = -1
-            if (self.obstacle_density >= 0) and (self.obstacle_density <= 100):
-                break
+        print("   Enter obstacle density in percentage: ", end='')
+        try:
+            self.obstacle_density = int(input())
+        except:
+            print("   Your input is wrong!!")
+            self.enter_obstacle_density()
+        if self.obstacle_density <0 or self.obstacle_density > 100:
+            print("   Your input is wrong!!")
+            self.enter_obstacle_density()
 
+    #asks predator number
     def enter_predator_number(self):
-        while True:
-            print("   Enter number of predators: ", end='')
-            try:
-                self.predator_number = int(input())
-            except:
-                self.predator_number = 0
-            if self.predator_number > 0:
-                break
+        print("   Enter number of predators: ", end='')
+        try:
+            self.predator_number = int(input())
+        except:
+            print("   Your input is wrong!!")
+            self.enter_predator_number()
+        if self.predator_number <=0:
+            print("   Your input is wrong!!")
+            self.enter_predator_number()
 
+    #asks prey number
     def enter_prey_number(self):
-        while True:
-            print("   Enter number of preys: ", end='')
-            try:
-                self.prey_number = int(input())
-            except:
-                self.prey_number = 0
-            if self.prey_number > 0:
-                break
+        print("   Enter number of preys: ", end='')
+        try:
+            self.prey_number = int(input())
+        except:
+            print("   Your input is wrong!!")
+            self.enter_prey_number()
+        if self.prey_number <=0:
+            print("   Your input is wrong!!")
+            self.enter_prey_number()
